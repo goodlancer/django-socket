1d905df4f68edeb4b4c8aaa0a31a19d1d9c121ae
goodlancer <forethink888@gmail.com>
2022-12-08 18:51:18 +0200

    Upload python work

---
+++ tests/gis_tests/geoadmin/models.py
@@ -0,0 +1,21 @@
+from django.contrib.gis.db import models
+
+from ..admin import admin
+
+
+class City(models.Model):
+    name = models.CharField(max_length=30)
+    point = models.PointField()
+
+    class Meta:
+        app_label = 'geoadmin'
+
+    def __str__(self):
+        return self.name
+
+
+site = admin.AdminSite(name='gis_admin_modeladmin')
+site.register(City, admin.ModelAdmin)
+
+site_gis = admin.AdminSite(name='gis_admin_gismodeladmin')
+site_gis.register(City, admin.GISModelAdmin)
